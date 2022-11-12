f41d86c59b6eb3a8b02e4730256bfe71e6dc9831
Yolopronoscop3r <aaa_13@o2.pl>
2022-11-12 23:34:15 +0100

    minMax v1.1

--- game.js
+++ game.js
@@ -25,9 +25,9 @@ class Player {
         this.red += points
     }
     typeOfPoints(src) {
-        let tmp = src.slice(7,9).split('')
+        let tmp = src.slice(7, 9).split('')
         //console.log(tmp)
-        return tmp[1] == "r" ? this.setRed(tmp[0]-0) : this.setGreen(tmp[0]-0)
+        return tmp[1] == "r" ? this.setRed(tmp[0] - 0) : this.setGreen(tmp[0] - 0)
     }
 }
 
@@ -69,6 +69,8 @@ let tbody = table.appendChild(document.createElement('tbody'))
 let field, tr, td, img, gamePlayed, dp1 //fp1
 let col = 1
 let fp2 = []
+let row = []
+let board = []
 //fp1 = Shuffle(images)
 //fp1 = fp1.indexOf('farmer.png')
 Shuffle(images)
@@ -82,11 +84,17 @@ for (let i = 0; i < images.length; i++) {
 }
 col = 1
 for (let i = 0; i < images.length; i++) {
-    images[i] = 'japka//' + images[i]
+    if (images[i] !== 'farmer.png') {
+        row.push({ y: col, x: ((i + 1) % 5) == 0 ? 5 : ((i + 1) % 5), value: images[i].slice(0, 1), color: images[i].slice(1, 2) })
+    } else {
+        row.push({ y: col, x: ((i + 1) % 5) == 0 ? 5 : ((i + 1) % 5), value: 'farmer', color: null })
+    }
+    //console.log(row)
     if (i % 5 == 0) tr = tbody.appendChild(document.createElement('tr'))
     field = tr.appendChild(document.createElement('td'))
     field.setAttribute('id', col + '' + (((i + 1) % 5) == 0 ? 5 : ((i + 1) % 5)))
     img = field.appendChild(document.createElement('img'))
+    images[i] = 'japka//' + images[i]
     img.src = images[i]
     img.setAttribute('alt', images[i])
     img.setAttribute('class', 'gameImages')
@@ -98,16 +106,36 @@ for (let i = 0; i < images.length; i++) {
     } else {
         img.parentElement.style.backgroundColor = '#7fbc98'
     }
-    if(images[i] === 'japka//farmer.png') {
+    if (images[i] === 'japka//farmer.png') {
         img.setAttribute('id', 'field-farmer')
         img.parentElement.style.backgroundColor = '#dbdbdb'
-    } else {img.setAttribute('id', `field-${col + '' + (((i + 1) % 5) == 0 ? 5 : ((i + 1) % 5))}`)}
+    } else { img.setAttribute('id', `field-${col + '' + (((i + 1) % 5) == 0 ? 5 : ((i + 1) % 5))}`) }
 
     img.setAttribute('onclick', "move('#" + img.id + "')")
-    if ((i + 1) % 5 == 0) col++
+    if ((i + 1) % 5 == 0) {
+        col++
+        board.push(row)
+        row = []
+    }
 }
 
+function getCurrentBoard() {
+    document.querySelectorAll('tr').forEach((row, index) => {
+        let i = 0
+        for (const td of row.children) {
+            //console.log(col)
+            if (td.firstChild.id == 'field-farmer') {
+                board[index][i] = { y: index + 1, x: i + 1, value: 'farmer', color: null }
+            } else if (td.firstChild.id == 'empty') {
+                board[index][i] = { y: index + 1, x: i + 1, value: null, color: null }
+            } else {
+                board[index][i] = { y: index + 1, x: i + 1, value: td.firstChild.alt.slice(7, 8), color: td.firstChild.alt.slice(8, 9) }
+            }
+            i++
+        }
 
+    })
+}
 
 function Shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -120,10 +148,10 @@ function Shuffle(array) {
 }
 
 function move(selector) {
-    
+
     if (selector == '#field-farmer' || !isLegalMove(selector)) return
     //console.log(selector)
-    paint(selector) 
+    paint(selector)
     if (isLegalMove(selector)) {
         document.querySelector('#field-farmer').parentElement.style.backgroundColor = '#7b7b7b'
     }
@@ -138,8 +166,9 @@ function move(selector) {
     img.src = 'japka//farmer.png'
     img.setAttribute('id', 'field-farmer')
     img.setAttribute('onclick', "move('#" + img.id + "')")
+    getCurrentBoard()
     play()
-}    
+}
 
 function isLegalMove(selector) {
     let fp = ('' + (document.querySelector('#field-farmer').parentElement.id)).split('')
@@ -157,15 +186,15 @@ function play() {
         if (currentPlayer === player1) {
             document.querySelector('#whichP').innerHTML = "Player 2's turn"
             currentPlayer = player2
-            if (isPVE) setTimeout(function () {getLegalMove()}, 500)
+            if (isPVE) makeBestMove()//setTimeout(function () { getLegalMove() }, 500)
         } else if (currentPlayer === player2) {
             document.querySelector('#whichP').innerHTML = "Player 1's turn"
             currentPlayer = player1
         }
         rounds++
     } else {
-        player1.isExactly11() || player2.isLose() 
-            ? info.innerHTML = 'Player 1 wins in: ' + rounds + ' rounds' 
+        player1.isExactly11() || player2.isLose()
+            ? info.innerHTML = 'Player 1 wins in: ' + rounds + ' rounds'
             : info.innerHTML = 'Player 2 wins in: ' + rounds + ' rounds'
         document.querySelectorAll('td').forEach(x => x.firstChild.setAttribute('onclick', null))
     }
@@ -186,7 +215,7 @@ function paint(selector) {
                 !(currentPlayer === player1) ? tds[i].style.backgroundColor = '#1f7c98' : tds[i].style.backgroundColor = '#C44E4E'
                 //tds[i].style.backgroundColor = '#1f7c98'
             } else {
-               tds[i].style.backgroundColor = '#7fbc98'
+                tds[i].style.backgroundColor = '#7fbc98'
             }
         }
         if ((i + 1) % 5 == 0) col++
@@ -203,18 +232,19 @@ let israndom = true
 let isminmax = false
 function PVE() {
     isPVE = true
+
 }
 
-function EVE() { 
-    !(player1.isPlay() && player2.isPlay() && !player1.isExactly11() && !player2.isExactly11()) 
-    ? location.reload(true) : EVELoop();
+function EVE() {
+    !(player1.isPlay() && player2.isPlay() && !player1.isExactly11() && !player2.isExactly11())
+        ? location.reload(true) : EVELoop();
 }
 function EVELoop() {
-    setTimeout(function () {   
-        getLegalMove()          
-        if (player1.isPlay() && player2.isPlay() && !player1.isExactly11() && !player2.isExactly11()) {           //  if the counter < 10, call the loop function
-            EVELoop()            
-        }                       
+    setTimeout(function () {
+        getLegalMove()
+        if (player1.isPlay() && player2.isPlay() && !player1.isExactly11() && !player2.isExactly11()) {
+            EVELoop()
+        }
     }, 500)
 }
 
@@ -237,19 +267,14 @@ function getLegalMove() {
     }
     //console.log(legal)
     legal = legal.filter(x => x > 0)
-    console.log(legal)
+    //console.log(legal)
     if (israndom) {
         selector = legal[Math.floor(Math.random() * legal.length)]
     } else if (isminmax) {
-        // let bestScore = -Infinity;
-        // let score = minMax(allP, 2, false);
-        // if (score > bestScore) {
-        //     console.log(bestScore)
-        //     selector = bestScore;
-        // }
+        makeBestMove()
     }
     //selector = legal[Math.floor(Math.random() * legal.length)]
-    move('#field-' + selector) 
+    move('#field-' + selector)
 }
 
 function isRandom() {
@@ -261,26 +286,108 @@ function isMinMax() {
     israndom = false
 }
 
-// function minMax(allP, depth, isMaximizing) {
-//     if (!(player1.isPlay() && player2.isPlay() && !player1.isExactly11() && !player2.isExactly11()) || depth == 0) {
-//         let score = 0
-//         player1.isExactly11() || player2.isLose() ? score = 1 : score = -1
-//         return score
-//     }
 
-//     if (isMaximizing) {
-//         let bestScore = -Infinity;
-//         for (let i = 0; i < legal.length; i++) {
-//             let score = minMax(legal, depth - 1, false);
-//             bestScore = Math.max(score, bestScore);
-//         }
-//         return bestScore;
-//     } else {
-//         let bestScore = Infinity;
-//         for (let i = 0; i < legal.length; i++) {
-//             let score = minMax(legal, depth - 1, true);
-//             bestScore = Math.min(score, bestScore);
-//         }
-//         return bestScore;
-//     }
-// }
+function minMax(board, depth, isMaximizing) {
+    let boardCopy = JSON.parse(JSON.stringify(board))
+    let legalMoves = getBoardLegalMoves()
+    if (evaluate() == Infinity || depth == 0) {
+        return evaluate()
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        let bestMove
+        let tmpMove
+        console.log(legalMoves)
+        legalMoves.forEach(move => {
+            let boardCopyCopy = JSON.parse(JSON.stringify(boardCopy))
+            let farmer = getFarmer(boardCopyCopy)
+            boardCopyCopy[farmer.y - 1][farmer.x - 1] = { y: farmer.y, x: farmer.x, value: null, color: null }
+            console.log(move)
+            boardCopyCopy[move.y - 1][move.x - 1] = { y: move.y, x: move.x, value: 'farmer', color: null }
+            tmpMove = move
+            let score = minMax(boardCopyCopy, depth - 1, false);
+            if (score > bestScore) {
+                bestScore = score
+                bestMove = tmpMove
+            }
+        })
+        return bestMove;
+    } else {
+        let bestScore = Infinity;
+        let bestMove
+        let tmpMove
+        legalMoves.forEach(move => {
+            let boardCopyCopy = JSON.parse(JSON.stringify(boardCopy))
+            let farmer = getFarmer(boardCopyCopy)
+            boardCopyCopy[farmer.y - 1][farmer.x - 1] = { y: farmer.y, x: farmer.x, value: null, color: null }
+            boardCopyCopy[move.y - 1][move.x - 1] = { y: move.y, x: move.x, value: 'farmer', color: null }
+            tmpMove = move
+            let score = minMax(boardCopyCopy, depth - 1, true);
+            if (score > bestScore) {
+                bestScore = score
+                bestMove = tmpMove
+            }
+        })
+        return bestMove;
+    }
+}
+
+function getFarmer(board) {
+    let farmer
+    board.forEach(y => {
+        y.forEach(x => {
+            if (x.value == 'farmer') {
+                farmer = x
+            }
+        })
+    })
+    return farmer
+}
+
+function getBoardLegalMoves() {
+    let farmer = getFarmer(board)
+    let legalMoves = []
+
+    for (let i = 0; i < 5; i++) {
+        if (board[i][farmer.x - 1].value !== null && board[i][farmer.x - 1].value !== 'farmer') {
+            legalMoves.push({ y: board[i][farmer.x - 1].y, x: board[i][farmer.x - 1].x })
+        }
+    }
+    for (let i = 0; i < 5; i++) {
+        if (board[farmer.y - 1][i].value !== null && board[farmer.y - 1][i].value !== 'farmer') {
+            legalMoves.push({ y: board[farmer.y - 1][i].y, x: board[farmer.y - 1][i].x })
+        }
+    }
+    return legalMoves
+}
+
+function evaluate() {
+    let score = 0
+    if (currentPlayer.getGreen() == 0 && currentPlayer.getRed() == 0) {
+        score += 1
+    }
+    if (player1.getGreen() > 11 && player1.getRed() > 11) {
+        return -Infinity
+    }
+    if (player1.getGreen() == 11 && player1.getRed() == 11) {
+        return Infinity
+    }
+    if (player2.getGreen() > 11 && player2.getRed() > 11) {
+        return Infinity
+    }
+    if (player2.getGreen() == 11 && player2.getRed() == 11) {
+        return -Infinity
+    }
+
+    // board.value
+    // board.color
+    // currentPlayer.getGreen()
+    // currentPlayer.getRed()
+    return score
+}
+
+function makeBestMove() {
+    let bestMove = minMax(board, 6, false)
+    console.log(bestMove)
+}
